bbf1c2a1e5aac751dff900f30ff26feb20693c09
kari.zhang <kari.zhang@qq.com>
2016-01-10 23:54:32 +0800

    Remove zlib-1.2.8

--- libs/zlib-1.2.8/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES :=$(notdir $(filter-out %/example.c, $(wildcard $(LOCAL_PATH)/*.c)))
-
-
-LOCAL_MODULE := libz
-#include $(BUILD_SHARED_LIBRARY)
-include $(BUILD_STATIC_LIBRARY)
